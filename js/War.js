794160d7b11d6d7bd95a65a1ae71dd04fdab4395
drejmin <djjamn@gmail.com>
2023-07-06 12:01:37 -0700

    modified:   css/war.css 	modified:   js/War.js 	modified:   war.html

--- js/War.js
+++ js/War.js
@@ -15,26 +15,8 @@ const shuffledContainer = document.getElementById('shuffled-deck-container');
 /*----- event listeners -----*/
 document.querySelector('button').addEventListener('click', renderNewShuffledDeck);
 
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
 /*----- functions -----*/
 
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-  document.getElementById("myDropdown").classList.toggle("show");
-}
 
 function getNewShuffledDeck() {
   // Create a copy of the originalDeck (leave originalDeck untouched!)
