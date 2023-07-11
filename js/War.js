9f4bb6e764b07e24cebe6c02cdcba227af97fcae
drejmin <djjamn@gmail.com>
2023-07-11 07:24:44 -0700

    modified:   js/War.js

--- js/War.js
+++ js/War.js
@@ -1,22 +1,9 @@
 /*----- constants -----*/
 const suits = ['s', 'c', 'd', 'h'];
 const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
-//const jranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A','joker-red','joker-black'];
-// const player1 = 'green';
-// const player2 = 'blue';
-// const player3 = 'yellow';
-// const player4 = 'red';
 
-
-// Build an 'original' deck of 'card' objects used to create shuffled decks
 const originalDeck = buildOriginalDeck();
-//renderDeckInContainer(originalDeck, document.getElementById('original-deck-container'));
 
-//const shuffledContainer = renderNewShuffledDeck();
-//renderDeckInContainer(shuffledDeck, document.getElementById('shuffled-deck-container'))
-
-//const jokerDeck = buildJokerDeck();
-//renderJokerDeckInContainer(jokerDeck, document.getElementById('joker-deck-container'))
 
 /*----- app's state (variables) -----*/
 let shuffledDeck = [];
@@ -30,51 +17,101 @@ let compHand = [];
 let playerCard = '', compCard = '';
 
 /*----- cached element references -----*/
-
-
-//const jokerContainer = document.getElementById('joker-deck-container');
-
-
 /*----- event listeners -----*/
-//document.querySelector('button').addEventListener('click', renderNewShuffledDeck);
-// document.querySelectorAll('checkbox').addEventListener('selected',rules);
-// document.querySelectorAll('rounds').addEventListener('selected',playRounds);
-// document.querySelectorAll('players').addEventListener('selected', numPlayers);
-
-
 /*----- functions -----*/
 
-//buildJokerDeck();
+window.onload = function() {$("#gameboard").hide()};
+
+function hideAll() {
+	$("#header").hide();
+	$("#gameboard").hide();
+	$("#numPlayers").hide();
+	$("#numRounds").hide();
+	$("#start").hide();
+  $('#ruleSet').hide();
+	$(".newGame").hide();
+
+
+}function renderBoard() {
+	hideAll();
+	$("#gameboard").show();
+	playing = true;
+  splitCards();
+}
 
 
 function splitCards() {
-
+  
   buildOriginalDeck();
   getNewShuffledDeck();
   let i=0
 	for (i=0; i!=26; i++) {
-
-    const rndIdx = Math.floor(Math.random() * shuffledDeck.length/2)
-
+    
     playerHand.push(shuffledDeck.splice(26,1));
 	}
   for (i=0; i!=26; i++) {
-
-    const rndIdx = Math.floor(Math.random() * shuffledDeck.length/2)
     
     compHand.push(shuffledDeck.splice(0,1));
   }
+  
+	// $('.playCount').html("Player cards: " + playerHand.length);
+	// $('.compCount').html("Computer cards: " + compHand.length);
+	// $('.result').html("");
+}
+
+function buildOriginalDeck() {
+  const deck = [];
+  // Use nested forEach to generate card objects
+  suits.forEach(function(suit) {
+    ranks.forEach(function(rank) {
+      deck.push({
+        // The 'face' property maps to the library's CSS classes for cards
+        face: `${suit}${rank}`,
+        
+        value: Number(rank) || (rank=== 'A' ? 14: rank === 'K' ? 13: rank === 'Q' ? 12: rank ==='J' ? 11:10)
+       
+      });
+    });
+  });
+  return deck;
+}
+
+function getNewShuffledDeck() {
+  // Create a copy of the originalDeck (leave originalDeck untouched!)
+  const tempDeck = [...originalDeck];
+  while (tempDeck.length) {
+    
+    // Get a random index for a card still in the tempDeck
+    const rndIdx = Math.floor(Math.random() * tempDeck.length/2);
+    
+    shuffledDeck.push(tempDeck.splice(rndIdx, 1)[0])
+    ;
+  }
+}
+
+function deal() {
 
 	$('.playCount').html("Player cards: " + playerHand.length);
 	$('.compCount').html("Computer cards: " + compHand.length);
 	$('.result').html("");
-}
+    
+  compare(playerCard, compCard);
+  $('.playerCard').html("");
+  $('.compCard').html("");
+  $('.newGame').show();
+
+  playerCard = playerHand[0];
+  compCard = compHand[0];
+  
+
+  $('.playerCard').append(`<img class="card ${playerCard[0].face}"></img>`);
+  $('.compCard').append(`<img class="card ${compCard[0].face}"></img>`);}
 
 
 
 function compare(playerCard, compCard) {
-	
-	if((playerCard % 13) > (compCard % 13)) {
+  
+  if((playerCard % 13) > (compCard % 13)) {
 	
 		$('.result').html("Player wins!")
 		
@@ -144,8 +181,8 @@ function compareWar(playerCard, compCard) {
 		
 		playerHand.push.apply(playerHand, warArray);
 
-		playerHand.push(comp);
-		playerHand.push(player);
+		playerHand.push(compCard);
+		playerHand.push(playerCard);
 		
 		playerHand.shift();
 		compHand.shift();
@@ -193,124 +230,7 @@ function checkWin() {
 	}
 }
 
-window.onload = function() {$("#gameboard").hide()};
-
-function hideAll() {
-	$("#header").hide();
-	$("#gameboard").hide();
-	$("#numPlayers").hide();
-	$("#numRounds").hide();
-	$("#start").hide();
-  $('#ruleSet').hide();
-	$(".newGame").hide();
 
 
-}function renderBoard() {
-	hideAll();
-	$("#gameboard").show();
-	playing = true;
-  splitCards();
-}
 
-// function rules(){
-
-// }
-
-// function playRounds(){
-
-// }
-
-// function numPlayers(){
-
-// }
-
-function getNewShuffledDeck() {
-  // Create a copy of the originalDeck (leave originalDeck untouched!)
-  const tempDeck = [...originalDeck];
-  while (tempDeck.length) {
-    
-    // Get a random index for a card still in the tempDeck
-    const rndIdx = Math.floor(Math.random() * tempDeck.length/2);
-    
-    shuffledDeck.push(tempDeck.splice(rndIdx, 1)[0])
-    ;
-  }
-}
-function renderDeckInContainer(deck, container) {
-  container.innerHTML = '';
-  // Let's build the cards as a string of HTML
-  let cardsHtml = '';
-  deck.forEach(function(card) {
-    cardsHtml += `<div class="card ${card.face}"></div>`;
-  });}
-
-//function renderNewShuffledDeck() {
-  // Create a copy of the originalDeck (leave originalDeck untouched!)
- // shuffledDeck = getNewShuffledDeck();
-  //renderDeckInContainer(shuffledDeck, shuffledContainer);
-//}
-
-// function renderJokerDeckInContainer(deck, container) {
-//   container.innerHTML = '';
-
-//   let cardHtml = '';
-//   deck.forEach(function(card) {
-//     cardHtml += `<div class="card ${card.face}"></div>`;
-//   });
-  
-//   container.innerHTML = cardHtml;
-// } 
-
-function buildOriginalDeck() {
-  const deck = [];
-  // Use nested forEach to generate card objects
-  suits.forEach(function(suit) {
-    ranks.forEach(function(rank) {
-      deck.push({
-        // The 'face' property maps to the library's CSS classes for cards
-        face: `${suit}${rank}`,
-        
-        value: Number(rank) || (rank=== 'A' ? 14: rank === 'K' ? 13: rank === 'Q' ? 12: rank ==='J' ? 11:10)
-       
-      });
-    });
-  });
-  return deck;
-}
-// function buildJokerDeck() {
-//   const deck = [];
-//   // Use nested forEach to generate card objects
-//   suits.forEach(function(suit) {
-//     ranks.forEach(function(rank) {
-//       deck.push({
-        
-//         face: `${suit}${rank}`,
-        
-//         value: Number(rank) || (rank=== 'A' ? 14: rank === 'K' ? 13: rank === 'Q' ? 12: rank ==='J' ? 11: rank === 'joker-black' ? 15: rank === 'joker-red' ? 15:10)
-       
-//       });
-//     });
-//   });
-//   return deck;
-// }
-
-
-  function deal() {
-    
-    compare(playerCard, compCard);
-    $('.playerCard').html("");
-    $('.compCard').html("");
-    $('.newGame').show();
-  
-    playerCard = playerHand[0];
-    compCard = compHand[0];
-    
-  
-    // let cardsHtml = '';
-    // shuffledDeck.forEach(function(card) {
-    // cardsHtml += `<div class="card ${card.face}"></div>`;
-
-    $('.playerCard').append(`<img class="card ${playerCard[0].face}"></img>`);
-    $('.compCard').append(`<img class="card ${compCard[0].face}"></img>`);}
-  
   
